e9227f7606e77bb0b0e76082bbe6537136fcff7e
Yuriy Volkov <sir.volkov@outlook.com>
2024-01-23 13:05:19 +0200

    upd: check point

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
+  Home,
+  Register,
+  Profile,
+  Login,
+  Contacts,
+  Info,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="register" element={<Register />}>
+            <Route path="profile" element={<Profile />} />
+          </Route>
+          <Route path="login" element={<Login />}>
+            <Route path="contacts" element={<Contacts />}>
+              <Route path="info" element={<Info />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
     </ChakraProvider>
   );
 }
 
 export default App;
+
+// {/* <Box textAlign="center" fontSize="xl">
+//   <Grid minH="100vh" p={3}>
+//     <ColorModeSwitcher justifySelf="flex-end" />
+//     <VStack spacing={8}>
+//       <Logo h="40vmin" pointerEvents="none" />
+//       <Text>
+//         Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+//       </Text>
+//       <Link
+//         color="teal.500"
+//         href="https://chakra-ui.com"
+//         fontSize="2xl"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn Chakra
+//       </Link>
+//     </VStack>
+//   </Grid>
+// </Box>; */}
